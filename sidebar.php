dcc3c60b91324be5ea1a13cc5d881460d2cdd56d
Sany khan <momin879@gmail.om>
2026-09-14 17:20:06 +0600

    Testing WP Pusher auto-deploy

--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
+ 
 /**
  * This file is here to avoid the Deprecated Message for sidebar by wp-includes/theme-compat/sidebar.php.
  */
